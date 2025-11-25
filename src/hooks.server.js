4500cc5dbc1746290361576f216f6eec1e892078
Atharv Kulkarni <atharvkulkarniofficial21@gmail.com>
2025-11-25 17:02:43 +0530

    hoock

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 
 export async function handle({ event, resolve }) {
 	const supabase = createClient(
-		import.meta.env.VITE_SUPABASE_URL,
-		import.meta.env.VITE_SUPABASE_ANON_KEY,
+		import.meta.env.SUPABASE_URL,
+		import.meta.env.SUPABASE_ANON_KEY,
 		{ auth: { persistSession: false } }
 	);
 
